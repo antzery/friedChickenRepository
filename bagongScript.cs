d2fabfd79298ab9caec28c6248d20169f078cff4
antzery <antzeryschool@gmail.com>
2024-12-16 17:56:12 +0800

    ito ang bagong skripts po
    no.

---
+++ bagongScript.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class bagongScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        print("The world is being hello-ed");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("H ell o W orld !");
+    }
+}
